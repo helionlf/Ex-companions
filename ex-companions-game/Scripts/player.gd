e8afebc35517e17c1ca7a7d620ba84874d4ebab1
Hélio Nogueira <116118382+helionlf@users.noreply.github.com>
2026-07-06 15:21:56 -0300

    projetil do player adicionado

--- ex-companions-game/Scripts/player.gd
+++ ex-companions-game/Scripts/player.gd
@@ -4,9 +4,10 @@ extends CharacterBody2D
 @export var speed = 100.0
 @export var health = 5
 
+@export var projetil: PackedScene
 
 func _physics_process(_delta: float) -> void:
-	handle_input()
+	handle_velocity()
 	move_and_slide()
 
 func attack():
@@ -16,8 +17,22 @@ func take_damage(amount):
 	print("jogador tomou dano")
 	health -= amount
 
-func handle_input():
+func handle_velocity():
 	var input := Vector2.ZERO
 	input = Input.get_vector("walk_feft", "walk_right", "walk_up", "walk_down")
 
 	velocity = input * speed
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		disparar_projetil()
+
+func disparar_projetil():
+	if projetil == null:
+		return
+	
+	var novo_projetil = projetil.instantiate()
+	var mouse_position = get_global_mouse_position()
+	var direction = (mouse_position - global_position)
+	get_tree().root.add_child(novo_projetil)
+	novo_projetil.iniciar(global_position, direction)
---
+++ ex-companions-game/Scripts/projetil_player.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var velocidade = 1000
+var direcao = Vector2.RIGHT 
+
+@onready var sprite_do_projetil = $AnimatedSprite2D
+
+
+func iniciar(posicao_global_inicial: Vector2, direcao_inicial: Vector2):
+	global_position = posicao_global_inicial
+	direcao = direcao_inicial.normalized()
+	
+	if direcao.x < 0:
+		sprite_do_projetil.flip_h = true
+	else:
+		sprite_do_projetil.flip_h = false
+
+
+func _process(delta):
+	position += direcao * velocidade * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+	queue_free()
