0d1bd2f3586d51c674571b2f46df25717b26c4af
Hélio Nogueira <116118382+helionlf@users.noreply.github.com>
2026-08-24 11:08:54 -0300

    adicionando ataque teste

--- ex-companions-game/Scripts/teste_companion.gd
+++ ex-companions-game/Scripts/teste_companion.gd
@@ -5,14 +5,17 @@ extends CharacterBody2D
 @export var attack_range = 30.0
 @export var player_follow_distance = 20.0 
 
+var enemy
+var player
+
 var random_attack = 0
 var current_attack = 0
 
 func _physics_process(_delta: float) -> void:
 	velocity = Vector2.ZERO
 
-	var enemy = get_tree().get_first_node_in_group("Enemy")
-	var player = get_tree().get_first_node_in_group("Player")
+	enemy = get_tree().get_first_node_in_group("Enemy")
+	player = get_tree().get_first_node_in_group("Player")
 
 	if is_instance_valid(enemy) and global_position.distance_to(enemy.global_position) < chase_range:
 		var distance_to_enemy = global_position.distance_to(enemy.global_position)
@@ -41,9 +44,10 @@ func choose_attack():
 		1:
 			pass
 		2:
-			pass
-			#var projetil
-			# Instanciar ataque de projetil
+			if enemy:
+				var projetil = preload("res://Testes/Companion_attack_projetil.tscn").instantiate
+				projetil.configurar_ataque("raio_psiquico", enemy)
+				add_child(projetil)
 		3:
 			pass
 		_:
--- ex-companions-game/Testes/companion_attack_projetil.gd
+++ ex-companions-game/Testes/companion_attack_projetil.gd
@@ -1,11 +1,43 @@
 extends AnimatedSprite2D
 
 
+
+var attack_name : String
+var target = PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	desativar_colisoes()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if attack_name and target:
+		lancar_projetil(attack_name, target)
+
+func lancar_projetil(name, target):
+	match name:
+		"raio_psiquico":
+			$colisoes/raio_psiquico.disabled = false
+			raio_psiquico(target)
+
+		"bola_de_fogo":
+			pass
+
+func raio_psiquico(target, speed = 20):
+	$".".play("attack_02")
+	
+	
+func boa_de_fogo():
 	pass
+
+func desativar_colisoes():
+	for c in $colisoes.get_children():
+		c.disabled = true
+
+func configurar_ataque(name, target_scn):
+	attack_name = name
+	target = target_scn
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
