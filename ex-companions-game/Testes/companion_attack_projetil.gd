38b93534e45316e1422c3e317a0ddb2edc3751e7
Hélio Nogueira <116118382+helionlf@users.noreply.github.com>
2026-08-25 09:53:08 -0300

    adicionando movimentação guiada, rotação e disparo reto para os projeteis

--- ex-companions-game/Testes/companion_attack_projetil.gd
+++ ex-companions-game/Testes/companion_attack_projetil.gd
@@ -14,9 +14,11 @@ func _process(delta: float) -> void:
 	if target:
 		if attack_name == "bola_de_fogo":
 			direction = global_position.direction_to(target.global_position)
+			look_at(target.position)
 		
 		# movimentação básica de movimento (em uma única direção)
 		position += direction * speed * delta
+		
 
 func choose_projetil():
 	match attack_name:
@@ -30,6 +32,7 @@ func choose_projetil():
 
 func raio_psiquico():
 	$".".play("attack_02")
+	set_angle_projetil()
 	#se tiver mais coisas alem de animação
 	
 func boa_de_fogo():
@@ -45,6 +48,9 @@ func configurar_ataque(attack_name_, target_, speed_):
 	speed = speed_
 	
 	direction = global_position.direction_to(target.global_position)
+
+func set_angle_projetil():
+	rotation = global_position.angle_to_point(target.global_position)
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
